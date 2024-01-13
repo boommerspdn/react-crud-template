c91ca5845241cc876e8491467a81e5f9a2235b4a
boommerspdn <131756488+boommerspdn@users.noreply.github.com>
2024-01-14 02:55:14 +0700

    fix

--- src/routes/about-page/page.tsx
+++ src/routes/about-page/page.tsx
@@ -1,8 +1,4 @@
-import { useGetPost } from "@/hooks/use-post";
-
 const AboutPage = () => {
-  const { data } = useGetPost("145bd5cd-4b0f-4841-9783-5fafdd996520");
-  console.log(data);
   return (
     <div className="text-3xl underline">
       <h1>AboutPage</h1>
